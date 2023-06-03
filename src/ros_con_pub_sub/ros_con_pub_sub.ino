2aedd5d497f33d088b55f032f2087bc9b24cdac1
exMachina316 <123073764+exMachina316@users.noreply.github.com>
2023-06-04 02:57:31 +0530

    Differential drive implimented

--- src/ros_con_pub_sub/ros_con_pub_sub.ino
+++ src/ros_con_pub_sub/ros_con_pub_sub.ino
@@ -3,7 +3,58 @@
 #include <std_msgs/String.h>
 #include <std_msgs/Int16.h>
 #include <std_msgs/Float64.h>
+#include <geometry_msgs/Twist.h>
 #include <Servo.h>
+
+int motor1p=D5;
+int motor1n=D6;
+int motor2p=D7;
+int motor2n=D8;
+
+float mapPwm(float x, float out_min, float out_max)
+{
+  return x * (out_max - out_min) + out_min;
+}
+
+
+float mapf(float x, float in_min, float in_max, float out_min, float out_max){
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+void onTwist(const geometry_msgs::Twist &msg){ 
+  float l = (msg.linear.x - msg.angular.z) / 2;
+  float r = (msg.linear.x + msg.angular.z) / 2;
+  
+  float lPwm = mapf(l, 0.0, 2.0, 0.0, 255.0);
+  float rPwm = mapf(r, 0.0, 2.0, 0.0, 255.0);
+  
+  // Set direction PWM on pins
+  if (l>0){
+    analogWrite(motor1p, lPwm);
+    analogWrite(motor1n, 0);
+  }
+  else{
+    analogWrite(motor1p, 0);
+    analogWrite(motor1n, lPwm);
+  }
+
+  if (r>0){
+    analogWrite(motor2p, rPwm);
+    analogWrite(motor2n, 0);
+  }
+  else{
+    analogWrite(motor2p, 0);
+    analogWrite(motor2n, rPwm);
+  }
+  
+  Serial.print(r);
+  Serial.print(":");
+  Serial.print(rPwm);
+  Serial.print(" ");
+  Serial.print(l);
+  Serial.print(":");
+  Serial.println(lPwm);
+}
 
 //////////////////////
 // WiFi Definitions //
@@ -56,7 +107,7 @@ void chatterCallback(const std_msgs::String& msg) {
   Serial.println(i);
 }
 
-ros::Subscriber<std_msgs::String> sub("message", &chatterCallback);
+ros::Subscriber<geometry_msgs::Twist> sub("/cmd_vel", &onTwist);
 std_msgs::String str_msg;
 ros::Publisher chatter("chatter", &str_msg);
 
@@ -81,11 +132,28 @@ void setupWiFi()
   wifi.init();
 }
 
+void stop()
+{
+  analogWrite(motor1p, 0);
+  analogWrite(motor1n, 0);
+  analogWrite(motor2p, 0);
+  analogWrite(motor2n, 0);
+}
+
 void setup() {
   Serial.begin(9600);
   setupWiFi();
   delay(2000);
   s.attach(2);  // PWM pin
+  pinMode(motor1p, OUTPUT);
+  pinMode(motor1n, OUTPUT);
+  pinMode(motor2p, OUTPUT);
+  pinMode(motor2n, OUTPUT);
+  
+  analogWrite(motor1p, 0);
+  analogWrite(motor1n, 0);
+  analogWrite(motor2p, 0);
+  analogWrite(motor2n, 0);
   nh.initNode();
   nh.subscribe(sub);
   nh.advertise(chatter);
